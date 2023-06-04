4e7fd118faa605059e135569fff8e47ef8911d82
ＩＴＳ ＪＯＥＬ <95572329+git-itsjoel@users.noreply.github.com>
2023-06-04 18:43:07 +0530

    Add files via upload

---
+++ autopicx/plugins/autopic.py
@@ -0,0 +1,66 @@
+#© 𝙄𝙩𝙨 ⚡ 𝙅𝙤𝙚𝙡 | #𝘼𝙗𝙊𝙪𝙩𝙈𝙚_𝘿𝙆
+
+from .. import client, TIME, CHANNEL_ID
+from telethon import events, types
+import logging 
+import os
+import asyncio
+from telethon.tl.functions.photos import UploadProfilePhotoRequest
+from telethon.tl.types import InputMessagesFilterPhotos
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+lock = asyncio.Lock()
+
+class temp(object):
+    CANCEL = False
+
+async def change_profile_pic(client):
+    channel_id = CHANNEL_ID
+    
+    async with lock:
+        while True:
+            if temp.CANCEL:
+                break
+            async for message in client.iter_messages(channel_id, reverse=True, filter=InputMessagesFilterPhotos):
+            
+                photo = await client.download_media(message=message.photo)
+                try:
+                    await client(UploadProfilePhotoRequest(file=await client.upload_file(f'{photo}')))
+                    os.remove(photo)
+               
+                except Exception as e:
+                    logger.exception(e)
+                    continue
+                await asyncio.sleep(TIME)
+        
+
+@client.on(events.NewMessage(outgoing=True, pattern='!cancel'))
+async def handle_cancel(event):
+    if not lock.locked():
+        msg = await event.respond('𝙉𝙤 𝙋𝙧𝙤𝙘𝙚𝙨𝙨 𝙍𝙪𝙣𝙣𝙞𝙣𝙜...')
+        await asyncio.sleep(30)
+        await msg.delete()
+        return
+    temp.CANCEL = True
+    msg = await event.respond('𝘾𝙖𝙣𝙘𝙚𝙡𝙞𝙣𝙜 𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓...')
+    await asyncio.sleep(30)
+    await msg.delete()
+
+@client.on(events.NewMessage(outgoing=True, pattern='!start'))
+async def handle_start(event):
+    temp.CANCEL = False
+    if lock.locked():
+        msg = await event.respond("𝘼𝙡𝙧𝙚𝙖𝙙𝙮 𝘼 𝙋𝙧𝙤𝙘𝙚𝙨𝙨 𝙄𝙨 𝙍𝙪𝙣𝙣𝙞𝙣𝙜......")
+        await asyncio.sleep(30)
+        await msg.delete()  
+        return      
+    try:
+        msg = await event.respond("𝙇𝙖𝙪𝙣𝙘𝙝𝙞𝙣𝙜 𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓......")
+        await change_profile_pic(client)
+        await asyncio.sleep(30)
+        await msg.delete()
+    except Exception as e:
+        logging.exception(e)
+        await event.respond(str(e))
---
+++ autopicx/plugins/ping.py
@@ -0,0 +1,45 @@
+#© 𝙄𝙩𝙨 ⚡ 𝙅𝙤𝙚𝙡 | #𝘼𝙗𝙊𝙪𝙩𝙈𝙚_𝘿𝙆
+
+from .. import client
+from telethon import events
+import logging 
+import asyncio
+import time
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# -- Constants -- #
+HELP = "**✨ Plugins ✨**\n\n`!start` - __To Start Changing DP__\n`!cancel` - __To Cancel Changing DP__\n`!alive` - __To Check If Bot Is Alive__\n`!repo` - __To Get The Repo__\n`!about` - __Details About Me__\n`!help` - __For This Message__"
+
+ABOUT_TXT = """
+**🌸 Name: 𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓
+📚 Library: [Telethon](https://docs.telethon.dev/)
+🀄 Language: [Python 3](https://www.python.org)
+⌨️ Dev: [𝙄𝙩𝙨 ⚡ 𝙅𝙤𝙚𝙡](https://t.me/joel_noob)
+✨ Inspiration: [Dᴋ 🇮🇳](https://t.me/AbOutMe_DK)**"""
+
+REPO = "https://github.com/git-itsjoel/AutoPicX"
+
+@client.on(events.NewMessage(outgoing=True, pattern='!repo'))
+async def repo(event):
+    await event.edit(REPO, link_preview=False)
+
+@client.on(events.NewMessage(outgoing=True, pattern='!about'))
+async def about(event):
+    await event.edit(ABOUT_TXT, link_preview=False)
+
+
+@client.on(events.NewMessage(outgoing=True, pattern='!help'))
+async def help_me(event):
+    await event.edit(HELP)
+
+
+@client.on(events.NewMessage(outgoing=True, pattern='!alive'))
+async def alive(event):
+    txt = await event.edit("▢▢▢")
+    await event.edit("▣▢▢")
+    await event.edit("▣▣▢")
+    await event.edit("▣▣▣")
+    await event.edit("𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚")
