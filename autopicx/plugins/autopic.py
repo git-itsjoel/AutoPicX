e3183a9064ee3384717185d1716ebf1c2e1f6afe
ＩＴＳ ＪＯＥＬ <95572329+git-itsjoel@users.noreply.github.com>
2023-06-10 16:30:55 +0530

    Update autopic.py

--- autopicx/plugins/autopic.py
+++ autopicx/plugins/autopic.py
@@ -6,7 +6,8 @@ import logging
 import os
 import asyncio
 from telethon.tl.functions.photos import UploadProfilePhotoRequest
-from telethon.tl.types import InputMessagesFilterPhotos
+from telethon.tl.types import InputMessagesFilterPhotos, InputPhoto
+from telethon.tl.functions.photos import DeletePhotosRequest
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -65,3 +66,10 @@ async def handle_start(event):
     except Exception as e:
         logging.exception(e)
         await event.respond(str(e))
+
+@client.on(events.NewMessage(outgoing=True, pattern='!delete'))
+async def handle_delete(event):
+    profile_photos = client.get_profile_photos('me')
+
+    for photo in profile_photos[1:]:
+            client(DeletePhotosRequest(id=[InputPhoto(id=photo.id)]))
