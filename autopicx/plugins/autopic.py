d45db6410ee97369f0c7f3512e170dc867b6da54
ＩＴＳ ＪＯＥＬ <95572329+git-itsjoel@users.noreply.github.com>
2023-06-11 16:05:44 +0530

    Update autopic.py

--- autopicx/plugins/autopic.py
+++ autopicx/plugins/autopic.py
@@ -31,7 +31,7 @@ async def change_profile_pic(client):
                 break
             async for message in client.iter_messages(channel_id, reverse=True, filter=InputMessagesFilterPhotos):
                 if ONE_DP:
-                    async for photo in client.iter_profile_photos("me", limit=1)
+                    async for photo in client.iter_profile_photos("me", limit=1):
                         await client(DeletePhotosRequest([photo]))
                 
 
