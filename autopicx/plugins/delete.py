ef14d54e5989a33c17eb7fee5bfb5877c1b83eb0
ＩＴＳ ＪＯＥＬ <95572329+git-itsjoel@users.noreply.github.com>
2023-06-13 06:01:40 +0530

    Update delete.py

--- autopicx/plugins/delete.py
+++ autopicx/plugins/delete.py
@@ -1,5 +1,6 @@
 import random
 import asyncio
+import datetime
 from pyrogram import Client, filters
 from .. import API_ID, API_HASH, PYRO_SESSION
 from .autopic import del_lock, lock, temp
@@ -23,6 +24,7 @@ if PYRO_SESSION is not None:
     
         async with del_lock:
             await message.edit("**Sᴛᴀʀᴛɪɴɢ Tᴏ Dᴇʟᴇᴛᴇ...**") 
+            start_time = datetime.datetime.now()
             deleted = 0
             remaining = await client.get_chat_photos_count("me")
             async for photo in client.get_chat_photos("me"):  
@@ -30,10 +32,12 @@ if PYRO_SESSION is not None:
                     break 
                 await client.delete_profile_photos([photo.file_id])
                 remaining-=1
-                deleted+=1
-                
+                deleted+=1                
+                current_time = datetime.datetime.now()
+                elapsed_time = current_time - start_time
+                time_remaining = elapsed_time / deleted * remaining
                 if deleted % 50 == 0:
-                    await message.edit(f"🗑️ Dᴇʟᴇᴛᴇᴅ: `{deleted}`\n🎗️ Rᴇᴍᴀɪɴɪɴɢ: `{remaining}`\n😴 Sʟᴇᴇᴘɪɴɢ: `120 sec`")
+                    await message.edit(f"**🗑️ Dᴇʟᴇᴛᴇᴅ: `{deleted}`\n🎗️ Rᴇᴍᴀɪɴɪɴɢ: `{remaining}`\n😴 Sʟᴇᴇᴘɪɴɢ: `120 sec`\n\n⏳ Tɪᴍᴇ Tᴀᴋᴇɴ: {elapsed_time}\n⏰ ETC: {time_remaining}**")
                     await asyncio.sleep(120)
                 else:
                     sleep = random.randint(1, 60)
